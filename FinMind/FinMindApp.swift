e31bcadfaa8dc2df8a2955aa4aa8fc23d6e54a13
Azat Ismagilov <azatzidane@gmail.com>
2025-09-02 21:28:10 +0300

    Обновил

--- FinMind/FinMindApp.swift
+++ FinMind/FinMindApp.swift
@@ -5,13 +5,18 @@ struct FinMindApp: App {
     @StateObject private var appState: AppState
 
     init() {
+        // Если Persistence.load() НЕ бросает ошибку (как в твоём проекте):
         let loaded = Persistence.shared.load() ?? AppState()
         _appState = StateObject(wrappedValue: loaded)
+
+        // Если твой load() всё-таки throws, используй вариант ниже и закомментируй 3 строки выше:
+        // let loaded = (try? Persistence.shared.load()) ?? AppState()
+        // _appState = StateObject(wrappedValue: loaded)
     }
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView() // <-- возвращаем твой корневой экран с вкладками
                 .environmentObject(appState)
                 .onAppear { appState.startAutoSave() }
         }
