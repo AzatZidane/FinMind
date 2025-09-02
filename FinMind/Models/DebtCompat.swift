8990cee9750160a228883f9024a07be108ec013a
Azat Ismagilov <azatzidane@gmail.com>
2025-09-02 20:47:09 +0300

    Обновил

--- FinMind/Models/DebtCompat.swift
+++ FinMind/Models/DebtCompat.swift
@@ -1,27 +1,36 @@
 import Foundation
 
+/// Старый формат ввода долгов
 enum DebtInput {
     case monthlyPayment(amount: Double, isMinimum: Bool)
     case loan(principal: Double, apr: Double, termMonths: Int, graceMonths: Int?, minPayment: Double?)
 }
 
 extension Debt {
+    /// Совместимый инициализатор из старого API
     init(name: String, input: DebtInput, currency: Currency = .rub) {
         switch input {
         case .monthlyPayment(let amount, _):
             self = Debt(id: UUID(), title: name, obligatoryMonthlyPayment: amount, currency: currency)
+
         case .loan(let principal, let apr, let termMonths, _, let minPayment):
+            // Аннуитетный платёж, если minPayment не задан
             let r = apr / 100.0 / 12.0
             let n = Double(termMonths)
-            let computed: Double
-            if let minPayment, minPayment > 0 {
-                computed = minPayment
+            let payment: Double
+            if let m = minPayment, m > 0 {
+                payment = m
             } else if r == 0 {
-                computed = principal / n
+                payment = principal / n
             } else {
-                computed = principal * r / (1.0 - pow(1.0 + r, -n))
+                payment = principal * r / (1.0 - pow(1.0 + r, -n))
             }
-            self = Debt(id: UUID(), title: name, obligatoryMonthlyPayment: computed, currency: currency)
+            self = Debt(id: UUID(), title: name, obligatoryMonthlyPayment: payment, currency: currency)
         }
+    }
+
+    /// Совместимое свойство для чтения `debt.input` в старых вью
+    var input: DebtInput {
+        .monthlyPayment(amount: obligatoryMonthlyPayment, isMinimum: true)
     }
 }
--- FinMind/Models/NameCompat.swift
+++ FinMind/Models/NameCompat.swift
@@ -1,30 +1,6 @@
 import Foundation
 
-// Совместимость со старыми вью: .name <-> .title
-extension Income {
-    var name: String {
-        get { title }
-        set { title = newValue }
-    }
-}
-
-extension Expense {
-    var name: String {
-        get { title }
-        set { title = newValue }
-    }
-}
-
-extension Goal {
-    var name: String {
-        get { title }
-        set { title = newValue }
-    }
-}
-
-extension Debt {
-    var name: String {
-        get { title }
-        set { title = newValue }
-    }
-}
+extension Income { var name: String { get { title } set { title = newValue } } }
+extension Expense { var name: String { get { title } set { title = newValue } } }
+extension Goal   { var name: String { get { title } set { title = newValue } } }
+extension Debt   { var name: String { get { title } set { title = newValue } } }
